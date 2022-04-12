47a081485bb4355876e5fd1d206e4abbe7aa77d7
abraham-kwizera <kwiz.abraham@gmail.com>
2022-04-12 14:45:13 +0200

    Disable next line, and remove console

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-let didShowMessage = false;
 const message = 'I like logs';
 
 function showMessage() {
     for (let i = 0; i < 5; i += 1) {
+        //eslint-disable-next-line no-console
         console.log(message);
     }
 }
